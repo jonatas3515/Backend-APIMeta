b8d2f292b68d4305b6f8a2d1399c913c89bb82e6
Jonatas Costa <jonatas3515@gmail.com>
2026-08-21 08:32:56 -0300

    Inclui binario ffmpeg via outputFileTracingIncludes do Next.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    outputFileTracingIncludes: {
+      '/api/send-message': [
+        './node_modules/ffmpeg-static/ffmpeg*',
+      ],
+    },
+  },
   async rewrites() {
     return [
       {
